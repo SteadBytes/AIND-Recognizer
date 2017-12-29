8266aba77f621b2d2fd8105b896cd2fbc6c7923b
Ben Steadman <=>
2017-12-29 11:17:22 +0000

    ASL Recognizer 1st term final project

--- my_recognizer.py
+++ my_recognizer.py
@@ -2,7 +2,7 @@ import warnings
 from asl_data import SinglesData
 
 
-def recognize(models: dict, test_set: SinglesData):
+def recognize(models: dict, test_set: SinglesData, num_guesses=1):
     """ Recognize test word sequences from word models set
 
    :param models: dict of trained models
@@ -15,11 +15,27 @@ def recognize(models: dict, test_set: SinglesData):
             {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
             ]
        guesses is a list of the best guess words ordered by the test set word_id
+       When num_guesses==1, 1d list: 
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+       When num_guesses > 1, 2d list of num_guesses for each word:
+           [['WORDGUESS0_0','WORDGUESS0_1'], ['WORDGUESS1_0','WORDGUESS0_2']]
+           Used for optional part 4 where choices for each word needed
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    for i in range(test_set.num_items):
+        X, Xlengths = test_set.get_item_Xlengths(i)
+        this_probs = {}
+        for word, model in models.items():
+            try:
+                this_probs[word] = model.score(X, Xlengths)
+            except:
+                pass
+        probabilities.append(this_probs)
+        if num_guesses == 1:
+            guesses.append(max(this_probs, key=lambda k: this_probs[k]))
+        else:
+            guesses.append(sorted(this_probs, key=this_probs.get,
+                                  reverse=True)[:num_guesses])
+    return probabilities, guesses
